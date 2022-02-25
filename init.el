7adad2362e3866a36bb66cb556b3a1cc0d2cdf49
hvsouza <henriquevieira.souza@gmail.com>
2022-02-25 11:16:56 +0100

    better mouse and custom

--- init.el
+++ init.el
@@ -1,4 +1,10 @@
-(setq inhibit-startup-message t)
+(setq inhibit-startup-message t
+      cursor-type 'bar
+      )
+
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
+
 (fset 'yes-or-no-p 'y-or-n-p)
 (global-set-key (kbd "<f5>") 'revert-buffer)
 
@@ -189,24 +195,6 @@
 
 
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(ansi-color-names-vector
-   ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#e090d7" "#8cc4ff" "#eeeeec"])
- '(custom-enabled-themes '(tango-dark))
- '(package-selected-packages
-   '(nyan-mode auto-complete counsel swiper org-bullets which-key whick-key try zygospore projectile company use-package)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
 
 ;; Line numbering
 (global-display-line-numbers-mode)
