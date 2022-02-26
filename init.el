86ce1a175c2f0afcad264604f9c9dc8b0b6a75b2
Henrique Souza PC casa <henriquevieira.souza@gmail.com>
2022-02-27 00:30:14 +0100

    not sure why git asked me to commit, lets see

--- init.el
+++ init.el
@@ -188,24 +188,6 @@
 
 
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(ansi-color-names-vector
-   ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#e090d7" "#8cc4ff" "#eeeeec"])
- '(custom-enabled-themes '(tango-dark))
- '(package-selected-packages
-   '(nyan-mode auto-complete counsel swiper org-bullets which-key whick-key try zygospore projectile company use-package)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
 
 ;; Line numbering
 (global-display-line-numbers-mode)
